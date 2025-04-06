0dc394e49fdcbd3628d0d6eb92cb11efc3364853
Viraj-Sankalpa <virajsankalpa@gmail.com>
2025-04-06 15:57:37 +0530

    uploading with sample data

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("menu-icon").addEventListener("click", function() {
+    var menuList = document.getElementById("menu-list");
+    if (menuList.style.display === "block") {
+        menuList.style.display = "none";
+    } else {
+        menuList.style.display = "block";
+    }
+});
+
+
+//Mail sender
